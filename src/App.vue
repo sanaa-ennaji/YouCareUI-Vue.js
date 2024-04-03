485cec7c128bbace0fe22a8ac176fab972753196
sanaa <sanaaennaji93@gmail.com>
2024-04-03 11:09:36 +0000

    add authontification

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<script setup>
+</script>
+
+<template>
+  <header>
+
+    <!-- <div class="wrapper">
+
+      <nav>
+        <RouterLink to="/" class="text-red-500">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div> -->
+
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+// import './assets/main.css'
+import './style.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
